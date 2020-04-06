6de321b650254d9c0316a52ed64d10823f32337c
Timothy Healy <healz@timothys-air.lan>
2020-04-05 23:55:22 -0700

    added shuffling tiles

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,14 +3,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-import Home from './home';
+import THGrid from './js/grid.js';
 import './app.css';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={THGrid} />
     </div>
     </BrowserRouter>
   );
