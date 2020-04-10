8c663d8315c684fdfcb622b2761b7972340af4c6
Tim Healy <timothy.healy@oath.com>
2020-04-10 13:46:35 -0700

    dynamically passing image from public folder

--- src/components/app/js/grid.js
+++ src/components/app/js/grid.js
@@ -7,18 +7,32 @@ import cards from './cards.js'
 import '../css/grid.css'
 
 function Card(props) {
-  const [flipped, set] = useState(true)
-  const { transform, opacity } = useSpring({
+    const [flipped, set] = useState(true)
+    const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
     config: { mass: 5, tension: 500, friction: 80 }
-  })
-  return (
-    <div class="card" onClick={() => set(state => !state)}>
-        <a.div class="c front" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }} />
-        <a.div class="c back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>{props.title}</a.div>
-    </div>
-  )
+    })
+    return (
+        <div class="card" onClick={() => set(state => !state)}>
+            <a.div
+                class="c front" 
+                style={{ 
+                    opacity,
+                    transform: transform.interpolate(t => `${t} rotateX(180deg)`),
+                    backgroundImage: `url(${process.env.PUBLIC_URL + 'images/' + props.image})`
+                }} 
+            />
+            <a.div
+                class="c back"
+                style={{
+                    opacity: opacity.interpolate(o => 1 - o),
+                    transform
+                }}
+            >{props.title}
+            </a.div>
+        </div>
+    )
 }
 
 class Grid extends Component {
