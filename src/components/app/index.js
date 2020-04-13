02366c5958706803890b93c5ef93d437579b3925
Tim Healy <timothy.healy@oath.com>
2020-04-13 15:53:58 -0700

    active page icon highlighting working

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -13,35 +13,44 @@ import About from '../About'
 import Posts from '../Posts';
 
 
-function App() {
-    return (
-        <BrowserRouter>
-            <div class="navbar">
-              <div class="head"><a href="/"><img src={process.env.PUBLIC_URL + 'images/headshot_small.jpg'}  alt=""/></a></div>
-              <div>
-                  <Link to="/posts">
-                      <i class="icon"><FontAwesomeIcon icon={faNewspaper} /></i>
-                  </Link>
-                      <Link to="/projects">
-                  <i class="icon"><FontAwesomeIcon icon={faCode} /></i>
-                  </Link>
-                  <a href="https://github.com/healyt22"><i class="icon"><FontAwesomeIcon icon={faGithub} /></i></a>
-                  <a href="https://twitter.com/healyt22"><i class="icon"><FontAwesomeIcon icon={faTwitter} /></i></a>
-                  <a href="https://linkedin.com/in/healyt22"><i class="icon"><FontAwesomeIcon icon={faLinkedin} /></i></a>
-              </div>
-            </div>
-            <div class="content">
-              <Switch>
-                  <Route exact path="/">
-                      <About />
-                  </Route>
-                  <Route path="/posts">
-                      <Posts />
-                  </Route>
-              </Switch>
-            </div>
-        </BrowserRouter>
-    );
-  }
+const classes = ['icon', 'icon'];
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            classes: classes,
+        };
+    }
+    
+    handleClick(i) {
+        const classesCopy = classes.slice();
+        classesCopy[i] = 'icon-active'
+        this.setState({classes: classesCopy});
+    };
+    
+    render() {
+        return (
+            <BrowserRouter>
+                <div class="navbar">
+                <div class="head"><a href="/"><img src={process.env.PUBLIC_URL + 'images/headshot_small.jpg'}  alt=""/></a></div>
+                <div class="icons">
+                    <Link to="/posts"><i class={this.state.classes[0]} onClick={() => this.handleClick(0)}><FontAwesomeIcon icon={faNewspaper} /></i></Link>
+                    <Link to="/projects"><i class={this.state.classes[1]} onClick={() => this.handleClick(1)}><FontAwesomeIcon icon={faCode} /></i></Link>
+                    <a href="https://github.com/healyt22"><i class="icon"><FontAwesomeIcon icon={faGithub} /></i></a>
+                    <a href="https://twitter.com/healyt22"><i class="icon"><FontAwesomeIcon icon={faTwitter} /></i></a>
+                    <a href="https://linkedin.com/in/healyt22"><i class="icon"><FontAwesomeIcon icon={faLinkedin} /></i></a>
+                </div>
+                </div>
+                <div class="content">
+                    <Switch>
+                        <Route exact path="/"><About /></Route>
+                        <Route path="/posts"><Posts /></Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
 
 export default App;
