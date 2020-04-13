53f13810ef6067b83137d326fa1460bcb282d1da
Tim Healy <timothy.healy@oath.com>
2020-04-13 16:38:46 -0700

    spacing is working i think

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,8 @@ function Intro() {
     return (
         <div class="about">
         <div class="intro">
-            <p>Hi, My name is <b>Tim Healy</b>. Welcome to my website! This where I post <b>personal projects</b> and develop my front-end chops. Recently, I finished an <b>M.S. in Applied Math</b> at Johns Hopkins University. Currently, I work as a <b>Data Engineer</b> for Verizon Media in the San Francisco Bay Area.</p>
+            <h1>Tim Healy</h1>
+            <p>Hi, welcome to my website! This where I post <b>personal projects</b> and develop my front-end chops. Recently, I finished an <b>M.S. in Applied Math</b> at Johns Hopkins University. Currently, I work as a <b>Data Engineer</b> for Verizon Media in the San Francisco Bay Area.</p>
         </div>
         <div class="spacer" />
         <div class="pic">
