5357ab4a5a4ecbd016f1ddd70cd494fdabbdb76f
Tim Healy <timothy.healy@oath.com>
2020-04-13 14:59:17 -0700

    spacer div added for about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,10 +4,12 @@ import './style.css';
 function Intro() {
     return (
         <div class="about">
-        <p class="intro">
-        Hi, My name is <b>Tim Healy</b>. Welcome to my website! This where I post <b>personal projects</b> and develop my front-end chops. Recently, I finished an <b>M.S. in Applied Math</b> at Johns Hopkins University. Currently, I work as a <b>Data Engineer</b> for Verizon Media in the San Francisco Bay Area.
-        </p>
-        <div><img src={process.env.PUBLIC_URL + 'images/tim_golf.jpg'} alt=""/></div>
+        <div class="intro">
+            <p>Hi, My name is <b>Tim Healy</b>. Welcome to my website! This where I post <b>personal projects</b> and develop my front-end chops. Recently, I finished an <b>M.S. in Applied Math</b> at Johns Hopkins University. Currently, I work as a <b>Data Engineer</b> for Verizon Media in the San Francisco Bay Area.</p>
+        </div>
+        <div class="spacer" />
+        <div class="pic">
+            <img src={process.env.PUBLIC_URL + 'images/tim_golf.jpg'} alt=""/></div>
         </div>
     );
 }
