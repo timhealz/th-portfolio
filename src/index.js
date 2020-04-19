43666b36e93c5769e4a4453a57deed31d501c4bd
Timothy Healy <healz@timothys-air.lan>
2020-04-19 15:43:16 -0700

    added scroll to top util

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 
+import ScrollToTop from './util/scrollToTop'
 import App from './components/app/';
 import * as serviceWorker from './serviceWorker';
 
@@ -9,6 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
+        <ScrollToTop />
             <App />
         </BrowserRouter>
     </React.StrictMode>,
