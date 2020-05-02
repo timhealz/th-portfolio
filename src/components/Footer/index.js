e42e17c04c11eabcda21a927653ba8cf8dbd6791
Timothy Healy <healz@timothys-air.lan>
2020-05-02 15:18:43 -0700

    reworking the navbar and changing the font

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './style.css';
 
+import IconLinks from '../IconLinks'
+
 function Footer() {
     return (
         <div className="footer">
             <p>Copyright &copy; {new Date().getFullYear()} Tim Healy</p>
+            <IconLinks />
         </div>
     );
 }
