22bb138b40fabad0cfd1b91d640a3cff734cca9b
Timothy Healy <healz@timothys-air.lan>
2020-05-02 15:18:43 -0700

    reworking the navbar and changing the font

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './style.css';
 
+import IconLinks from '../IconLinks'
+
 function Intro() {
     return (
         <div className="about">
             <div className="intro">
-                <h1>Tim Healy</h1>
+                <div className="top">
+                    <h1>Tim Healy</h1>
+                    <IconLinks />
+                </div>
                 <p>Hi, welcome to my website! This where I post <b>personal projects</b> and develop my front-end chops. Recently, I finished an <b>M.S. in Applied Math</b> at Johns Hopkins University. Currently, I work as a <b>Data Engineer</b> for Verizon Media in the San Francisco Bay Area.</p>
             </div>
             <div className="spacer" />
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,46 +2,34 @@ import React from 'react';
 import { Link, useLocation } from "react-router-dom";
 import './style.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
-import { faNewspaper } from '@fortawesome/free-regular-svg-icons'
 
 function Navbar() {
     let path = useLocation().pathname;
-    let navText = mapNavText()
-
-    function mapNavText(location) {
-        if      (path === "/")                  { return "Home" }
-        else if (path.startsWith("/posts"))     { return "Posts" }
-        else if (path.startsWith("/projects"))  { return "Projects" }
-        else                                    { return "Where are you?" }
-    }
 
     return (
         <div className="navbar">
-            <Link to="/"><img src={process.env.PUBLIC_URL + 'images/headshot_small.jpg'}  alt="Home"/></Link>
-            <div className="menu">
-                <div className="navbox">
-                    <h4 className="navtext">{navText}</h4>
-                </div>
-                <div className="icons">
-                    <Link to="/posts">
-                        <FontAwesomeIcon icon={faNewspaper} className={path.startsWith("/posts") ? "active-icon" : "icon"} />
-                    </Link>
-                    <Link to="/projects">
-                        <FontAwesomeIcon icon={faCode} className={path.startsWith("/projects") ? "active-icon" : "icon"} />
-                    </Link>
-                    <a href="https://github.com/healyt22">
-                        <FontAwesomeIcon icon={faGithub} className="icon" />
-                    </a>
-                    <a href="https://twitter.com/healyt22">
-                        <FontAwesomeIcon icon={faTwitter} className="icon" />
-                    </a>
-                    <a href="https://linkedin.com/in/healyt22">
-                        <FontAwesomeIcon icon={faLinkedin} className="icon" />
-                    </a>
-                </div>
+        <div className="picname">
+            <Link to="/">
+                <img src={process.env.PUBLIC_URL + 'images/headshot.jpg'}
+                alt="Home"/>
+            </Link>
+            </div>
+            <div className="navbox">
+                <Link to="/">
+                    <h3 className={
+                        path == "/" ? "active-navLink" : "navLink"
+                    }>Home</h3>
+                </Link>
+                <Link to="/posts">
+                    <h3 className={
+                        path.startsWith("/posts") ? "active-navLink" : "navLink"
+                    }>Posts</h3>
+                </Link>
+                <Link to="/projects">
+                    <h3 className={
+                        path.startsWith("/projects") ? "active-navLink" : "navLink"
+                    }>Projects</h3>
+                </Link>
             </div>
         </div>
     );
