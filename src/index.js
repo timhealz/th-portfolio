a3319f4a5f415fb4caecc12c12a2df413aa59740
Timothy Healy <healz@timothys-air.lan>
2020-05-02 12:27:25 -0700

    more code cleanup

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 
 import ScrollToTop from './util/scrollToTop'
-import App from './components/app/';
 import * as serviceWorker from './serviceWorker';
 
+import Navbar from './components/Navbar'
+import ContentRouter from './components/ContentRouter'
+import Footer from './components/Footer'
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
         <ScrollToTop />
-            <App />
+            <Navbar />
+            <ContentRouter />
+            <Footer />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('app')
